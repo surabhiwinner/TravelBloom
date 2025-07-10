ac6a47654b5a42b407cddc969293129580e15b3c
surabhiwinner <0104surabhi@gmail.com>
2025-07-10 14:47:17 +0000

    test

--- travelbloom/travelbloom/settings.py
+++ travelbloom/travelbloom/settings.py
@@ -162,7 +162,9 @@ PLACES_NEW_API = config('PLACES_NEW_API_KEY_')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
