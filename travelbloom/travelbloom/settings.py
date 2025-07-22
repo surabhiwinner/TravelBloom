0793af851ebf2ab64fcb46ed2857e1542b7e8f18
surabhiwinner <0104surabhi@gmail.com>
2025-07-22 20:31:59 +0000

    hosting

--- travelbloom/travelbloom/settings.py
+++ travelbloom/travelbloom/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config('_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SmartVoyegerSurabhi.pythonanywhere.com']
 
 
 # Application definition
