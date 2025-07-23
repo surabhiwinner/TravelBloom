eb3dfc223394bf7a7e7d32128d3f691cc12fadc5
surabhiwinner <0104surabhi@gmail.com>
2025-07-23 03:25:44 +0000

    Added Procfile for Render deployment

--- travelbloom/travelbloom/settings.py
+++ travelbloom/travelbloom/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config('_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
